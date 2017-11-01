ef816122b9fcf00d58b06f9afe9073839b26e955
Alexander Krause <akr@informatik.uni-kiel.de>
2017-11-01 15:38:01 +0100

    not hot deploy for the moment and cleanup BaseModel

--- src/main/java/net/explorviz/extension/dummy/model/BaseModel.java
+++ src/main/java/net/explorviz/extension/dummy/model/BaseModel.java
@@ -2,14 +2,19 @@ package net.explorviz.extension.dummy.model;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.github.jasminb.jsonapi.LongIdHandler;
 import com.github.jasminb.jsonapi.annotations.Id;
 
+//Needed for cyclical serialization
+@JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class, property = "id")
 public class BaseModel {
 
 	private static final AtomicLong ID_GENERATOR = new AtomicLong();
 
-	@Id
-	private long id;
+	@Id(LongIdHandler.class)
+	private Long id;
 
 	public BaseModel() {
 		id = ID_GENERATOR.incrementAndGet();
